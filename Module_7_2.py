55709bd774fa206936d25e9c1e3b8a329440db76
slash-arkh <slash-arkh@mail.ru>
2024-07-21 20:21:43 +0300

    Update Module_7_2.py

--- Module_7_2.py
+++ Module_7_2.py
@@ -7,9 +7,12 @@ def custom_write(file_name, strings):
         num += 1
         tuple_ = num, file.tell()
         strings_positions = {}
+        strings_positions_ = tuple_ , i
         strings_positions[tuple_]=  i
         file.write(i + '\n')
-        print(strings_positions)
+        print(strings_positions_)
+    return strings_positions
+
     file.close()
 
 
